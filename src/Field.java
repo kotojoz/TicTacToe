7defc72b941593b8dec2e57ae43f2b765ccd9bc3
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-09-02 21:20:31 +0300

    created win and draw methods

--- src/Field.java
+++ src/Field.java
@@ -47,4 +47,5 @@ public class Field {
         }
         System.out.println("---------");
     }
+
 }
--- src/Game.java
+++ src/Game.java
@@ -11,6 +11,10 @@ public class Game {
 
     private Player player2;
 
+    protected boolean isWin_X;
+
+    protected boolean isWin_O;
+
     private static Map<String, Player> players;
 
     public Game() {
@@ -29,6 +33,49 @@ public class Game {
 
     private void startNewGame() {
         field = new Field();
+        isWin_X = false;
+        isWin_O = false;
+    }
+
+    private boolean isGameOver() {
+        if (isWin_X) {
+            System.out.println("X wins");
+            return true;
+        } else if (isWin_O) {
+            System.out.println("O wins");
+            return true;
+        } else if (noEmptyCells()) {
+            System.out.println("Draw");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void analyzeGame() {
+        checkResult(field.grid.get(1).getMark() + field.grid.get(2).getMark() + field.grid.get(3).getMark());
+        checkResult(field.grid.get(4).getMark() + field.grid.get(5).getMark() + field.grid.get(6).getMark());
+        checkResult(field.grid.get(7).getMark() + field.grid.get(8).getMark() + field.grid.get(9).getMark());
+
+        checkResult(field.grid.get(1).getMark() + field.grid.get(4).getMark() + field.grid.get(7).getMark());
+        checkResult(field.grid.get(2).getMark() + field.grid.get(5).getMark() + field.grid.get(8).getMark());
+        checkResult(field.grid.get(3).getMark() + field.grid.get(6).getMark() + field.grid.get(9).getMark());
+
+        checkResult(field.grid.get(1).getMark() + field.grid.get(5).getMark() + field.grid.get(9).getMark());
+        checkResult(field.grid.get(3).getMark() + field.grid.get(5).getMark() + field.grid.get(7).getMark());
+
+    }
+
+    protected void checkResult(int result) {
+        if (result == 264) {
+            isWin_X = true;
+        } else if (result == 237) {
+            isWin_O = true;
+        }
+    }
+
+    private boolean noEmptyCells() {
+        return field.grid.values().stream().noneMatch(v -> v.getMark() == field.EMPTY_CELL);
     }
 
     public void menu() {
@@ -49,11 +96,21 @@ public class Game {
 
     public void playGame() {
         startNewGame();
+
         while (true) {
             player1.makeAMove(field, field.X);
             field.printField();
+            analyzeGame();
+            if (isGameOver()) {
+                break;
+            }
+
             player2.makeAMove(field, field.O);
             field.printField();
+            analyzeGame();
+            if (isGameOver()) {
+                break;
+            }
         }
     }
 }
